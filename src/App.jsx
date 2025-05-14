5526fa67c1c18ba3db1e987d18cf1b7872e024f9
CarlC <carlcastillano2000@gmail.com>
2025-05-14 21:08:59 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import "./index.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import { useState } from "react";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <>
+      {!isLoading && (
+        <LoadingScreen
+          onComplete={() => {
+            setIsLoading(true);
+          }}
+        />
+      )}
+    </>
+  );
+}
+
+export default App;

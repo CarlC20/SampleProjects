47b18f884a9adcf7f82d6f75192cfaf45788d422
CarlC <carlcastillano2000@gmail.com>
2025-05-25 18:21:18 +0800

    Added blinking CSS Added Navbar and Mobile Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/NavBar";
+import { MobileMenu } from "./components/MobileMenu";
 import { useState } from "react";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       {!isLoading && (
@@ -14,6 +17,15 @@ function App() {
           }}
         />
       )}
+
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoading ? "opacity-100" : "opcaity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
     </>
   );
 }
